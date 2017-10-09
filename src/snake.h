9a1985e5d7b0340a13c530cf1dc89900bddfeead
Madeleine Rapp <madra484@student.liu.se>
2017-10-09 11:51:24 +0200

    calcResp function addes

--- src/snake.h
+++ src/snake.h
@@ -8,25 +8,29 @@
 class Snake
 {
 public:
-  std::string name = "MyLitteBot";
-  std::string get_next_move(nlohmann::json map);
-  void on_game_ended();
-  void on_tournament_ended();
-  void on_snake_dead(std::string death_reason);
-  void on_game_starting();
-  void on_player_registered();
-  void on_invalid_playername();
-  void on_game_result(nlohmann::json playerRanks);
-  void InitializeCurves();
+	std::string name = "MyLitteBot";
+	std::string get_next_move(nlohmann::json map);
+	void on_game_ended();
+	void on_tournament_ended();
+	void on_snake_dead(std::string death_reason);
+	void on_game_starting();
+	void on_player_registered();
+	void on_invalid_playername();
+	void on_game_result(nlohmann::json playerRanks);
+	
+	void initializeCurves();
+	void calculateRespons(int direction);
 
 	int mySnakeSlot;
 
-  // Our curves 
+	// Our curves 
 	std::array<double,5> curveFood;
 	std::array<double,5> curveWall;
 	std::array<double,5> curvePlayers;
 	std::array<double,5> curveTail;
 	std::array<double,5> curveObstacle;
+
+	std::array<double,4> responsValue;
 };
 
 
